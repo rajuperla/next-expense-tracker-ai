4030424f272b29eb387829522fc5d6759666beed
raju240314 <raju240314@gmail.com>
2025-09-02 17:43:26 +0530

    About and Contact page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { currentUser } from '@clerk/nextjs/server'
+import Guest from '@/components/Guest'
 
-export default function HomePage() {
+export default async function HomePage() {
+  const user = await currentUser()
+
+  if (!user) {
+    return <Guest />
+  }
+
   return (
     <div>Home Page</div>
   )
